1fbc3291adb55fe7bfb82d9f00860fb2506c2839
MaratIssatayev <marat.issatayev06@gmail.com>
2026-08-26 19:49:12 +0500

    Create AboutUs component for nursery information
    Added AboutUs component with description of the nursery.

---
+++ AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1 className="about-us-heading">About Us</h1>
+      <p className="about-us-description">
+        Welcome to Paradise Nursery, your number one source for all green and house plants. We are dedicated to giving you the very best plant shopping experience, with a focus on quality, customer service, and uniqueness.
+      </p>
+      <p className="about-us-content">
+        Our team of plant enthusiasts works hard to curate a wide variety of indoor plants, succulents, and gardening tools to bring life and green spaces into your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
